f5c8ed18785e232f7cfa4f79361ea7b9a02356ac
Андрей <andrei.bagan2@mail.ru>
2021-06-23 12:46:54 +0300

    Добавьте файлы проекта.

---
+++ Heartbeat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+namespace SimpleHeartbeatService
+{
+    public sealed class Heartbeat
+    {
+        private readonly Timer _timer;
+
+        public Heartbeat()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"E:\visual studio\visual projects csharp\2021\HeartbeatServiceDeploy\Heartbeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
